a565aaf76349c4a88c6cef07e10dee72dba43ffa
Mayank <mayank525.work@gmail.com>
2025-04-20 22:43:01 +0530

    add auth-routes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from "express"
-import { login, logout, signup } from "../controllers/auth.controllers.js";
+import { getMe, login, logout, signup } from "../controllers/auth.controller.js";
+import { protectedRoutes } from "../middleware/protectedRoutes.js";
 
 const router = express.Router();
 
@@ -7,5 +8,6 @@ const router = express.Router();
 router.post("/signup", signup)
 router.post("/login", login)
 router.post("/logout", logout)
+router.get("/me", protectedRoutes, getMe)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js"
 import connectMongoDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 8000
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser())
+
 app.use("/api/auth", authRoutes)
 
+
 connectMongoDB().then(() => {
-        app.listen(8000, () => {
-            console.log("Server is running on port 8000");
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
         })
     }
 )
